11e5752cb81d6c6cd557b6e007f77cad66d8b28d
cayuval <jubim1234@gmail.com>
2022-06-07 15:51:29 +0300

    generate ID function

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hi');
+function addTax(price){
+    if(!price||price<=0||typeof(price)==='string'){
+        throw Error('price isnt valid') 
+    }
+        const vat = 1.17 //maam
+        const total = (price*vat).toFixed(2)
+        return Number(total);
+}
+
+function generateId(){
+    // const random = Math.random();
+    // console.log(`rand: ${random}`)
+    // const num = random*10000
+    // console.log(`num: ${num}`)
+    // const floor = Math.floor(num)
+    // console.log(`final: ${floor}`)
+    // return floor;
+
+    const time = new Date().getTime()
+    return Math.floor(Math.random()*time)
+}
