0ae6f071fe8f7320e0deeba7e3c6ff990eb00a86
cayuval <jubim1234@gmail.com>
2022-06-09 15:48:51 +0300

    better if

--- EX - Galleria/gallery.js
+++ EX - Galleria/gallery.js
@@ -2,44 +2,43 @@ const gallery = [
     {
         image: '/public/birds.png',
         name: 'A Great picture',
-        author:'Bruno Brave',
+        author: 'Bruno Brave',
         price: 200,
-        inStock:true
+        inStock: true
     },
     {
         image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
         name: 'Another Picture',
-        author:'Bruno Brave',
+        author: 'Bruno Brave',
         price: 360,
-        inStock:true
+        inStock: false
     },
     {
         image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
         name: 'Morning Good',
-        author:'Carlla Uno',
+        author: 'Carlla Uno',
         price: 295,
-        inStock:true
+        inStock: true
     },
     {
         image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
         name: 'Old Picture',
-        author:'Camilla Hart',
+        author: 'Camilla Hart',
         price: 522,
-        inStock:false
+        inStock: false
     },
 ]
 
 
 function getProductsTemplate() {
     let html = ''
-    let stockHtml =''
+    let stockHtml = ''
 
     // const IMG_PATH = 'https://cdn.pixabay.com/photo'//configuration
     gallery.forEach(product => {
-        if(product.inStock===false){
-            stockHtml='not aviable'
-        }else{
-            stockHtml=''
+        stockHtml = ''
+        if (product.inStock === false) {
+            stockHtml = 'not aviable'
         }
         html += `<div class="card" style="margin:5px;width: 18rem;">
                     <img src="${product.image}" class="card-img-top" alt="">
@@ -47,7 +46,7 @@ function getProductsTemplate() {
                         <h5 class="card-title">${product.name}</h5>
                         <h6 class="card-title">${product.author}</h6>
                         <p class="card-text">$${product.price}</p>
-                        <span style="color:red;" class="card-text">${stockHtml}<span>
+                        <span class="card-text text-danger">${stockHtml}<span>
                     </div>
                 </div>`;
     })
