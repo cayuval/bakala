1fc00685622d1bbc70cdadf41b34b0fb6a7e325c
cayuval <jubim1234@gmail.com>
2022-06-09 13:14:54 +0300

    improve products func

--- app.js
+++ app.js
@@ -23,33 +23,43 @@ function generateId() {
 
 const products = [
     {
-        id: 1,
+        id: generateId(),
         price: 15,
         name: 'Yellow Cheese',
-        image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        image: '/2016/03/05/19/24/cheese-1238395_960_720.jpg',
         description: 'great product really!'
     },
     {
-        id: 2,
+        id: generateId(),
         price: 3,
         name: 'ice cream',
-        image: 'https://cdn.pixabay.com/photo/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        image: '/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
         description: 'be carefull - very cold!'
-    } 
+    }
 ]
 
-function displayProduct(){
-    const container = document.getElementById('products-container')
+function getProductsTemplate() {
+    let html = ''
+    const IMG_PATH = 'https://cdn.pixabay.com/photo'//configuration
 
-    products.forEach(product=>{
-        container.innerHTML += `<div class="card" style=" margin:5px; width: 18rem;">
-                    <img src="${product.image}" class="card-img-top" alt="${product.description}">
+    products.forEach(product => {
+        html += `<div class="card" style="margin:5px;width: 18rem;">
+                    <img src="${IMG_PATH}${product.image}" class="card-img-top" alt="${product.description}">
                     <div class="card-body">
                         <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">${product.price}$</p>
+                        <p class="card-text">$${addTax(product.price)}</p>
                         <a href="#" class="btn btn-primary">View Details</a>
                     </div>
-                </div>`
+                </div>`;
     })
+    return html
+
 }
+
+function displayProduct() {
+    const container = document.getElementById('products-container')
+    const html = getProductsTemplate()
+    container.innerHTML = html;
+}
+
 displayProduct()
