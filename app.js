e27da3a88695acd58a15f52d212e04b361362bd3
cayuval <jubim1234@gmail.com>
2022-06-17 20:12:29 +0300

    end of the day 16:00

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const products = [
     {
         id: generateId(),
         price: 15,
-        name: 'Yellow Cheese',
+        name: 'yellow cheese',
         image: '/2016/03/05/19/24/cheese-1238395_960_720.jpg',
         description: 'great product really!'
     },
@@ -35,18 +35,38 @@ const products = [
         name: 'ice cream',
         image: '/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
         description: 'be carefull - very cold!'
-    }
+    },
+    {
+        id: generateId(),
+        price: 5,
+        name: 'aaaa',
+        image: '/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        description: 'aaaaaaaaaaaa'
+    },
 ]
 
-function getTotalProduct() {
-    return products.length
+function searchProduct() {
+    const userSearch = document.getElementById('searchInput').value.trim();
+    const searchResult = products.filter(product => {
+        return product.name.toLowerCase().includes(userSearch.toLowerCase())
+    })//brings an Object from The array!!!
+    console.log(userSearch);
+    console.log(searchResult);
+    displayProduct(searchResult)
 }
 
-function getProductsTemplate() {
+function getTotalProduct(productsData) {
+    return productsData.length
+}
+
+function getProductsTemplate(productsData) {
+    // at the beginning there was nothing
     let html = ''
+    // same thing for all pictures...
     const IMG_PATH = 'https://cdn.pixabay.com/photo'//configuration
 
-    products.forEach(product => {
+    // runs on the specific specified array and craeting an html for all products together
+    productsData.forEach(product => {
         html += `<div class="card" style="margin:5px;width: 18rem;">
                     <img src="${IMG_PATH}${product.image}" class="card-img-top" alt="${product.description}">
                     <div class="card-body">
@@ -56,14 +76,19 @@ function getProductsTemplate() {
                     </div>
                 </div>`;
     })
+    // return the html block
     return html
 }
 
-function displayProduct() {
+function displayProduct(productsData = products) {
+    // catch container
     const container = document.getElementById('products-container')
-    const productHtml = getProductsTemplate()
+    // getting HTML template for the products
+    const productHtml = getProductsTemplate(productsData)
+    // inject the template to the container
     container.innerHTML = productHtml;
-    document.getElementById('toatal-products').innerHTML = `(${getTotalProduct()})`
+    // catch total products template and inject the number of object
+    document.getElementById('toatal-products').innerHTML = `(${getTotalProduct(productsData)})`
 }
 
-displayProduct()
+displayProduct(products)
